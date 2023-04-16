d927df8062f8bee60e52434aa32fbf947921702f
AArt1256 <aart1256@outlook.com>
2023-04-16 23:27:48 +0300

    adding simple pwa support

--- js/other/windowStack.js
+++ js/other/windowStack.js
@@ -15,9 +15,6 @@ function windowStack(element) {
 		this.hookedMouse = false;
 		var thisObj2 = this;
 		this.events = [
-			["mousedown", function (event) { thisObj2.dragEnable(event); }],
-			["mousemove", function (event) { thisObj2.drag(event); }],
-			["mouseup", function (event) { thisObj2.dragDisable(); }]
 		];
 	}
 }
@@ -55,58 +52,6 @@ windowStack.prototype.getStyleFloatOf = function (propertyNameOf, JSpropertyName
 	var cleanValue = parseFloat((dirtyValue != null && dirtyValue.length) ? dirtyValue : "0");
 	return (!isNaN(cleanValue) && cleanValue != -1) ? cleanValue : 0;	/*Note: Opera gives -1, not NaN for cleanValue when no number is found.*/
 }
-windowStack.prototype.dragEnable = function (event) {
-	var elementAt = document.elementFromPoint(event.clientX, event.clientY);
-	this.leftPos = this.getStyleFloatOf("left", "left");
-	this.topPos = this.getStyleFloatOf("top", "top");
-	var highX = this.leftPos + this.domObject.offsetWidth - document.documentElement.scrollLeft - document.getElementsByTagName("body")[0].scrollLeft;
-	var highY = this.topPos + this.domObject.offsetHeight - document.documentElement.scrollTop - document.getElementsByTagName("body")[0].scrollTop;
-	var mousedWindow = isSameNode(elementAt, this.domObject);
-	this.mousedInsideWindow = (mousedWindow) ? false : isDescendantOf(this.domObject, elementAt);
-	if (mousedWindow || this.mousedInsideWindow) {
-		this.movable = true;
-		this.leftLast = event.clientX;
-		this.topLast = event.clientY;
-		this.domObject.style.zIndex = 2;
-		if (highX - this.leftLast < 25 && highY - this.topLast < 25) {	/*Inivisible resize corner area for windows.*/
-			this.resize = true;
-		}
-		else {
-			this.resize = false;
-		}
-		this.coordDiffLeft = this.leftLast - this.leftPos;
-		this.coordDiffTop = this.topLast - this.topPos;
-		this.lastLeft = 0;
-		this.lastTop = 0;
-	}
-	else {
-		this.lostFocus();
-	}
-}
-windowStack.prototype.drag = function (event) {
-	if (this.movable) {
-		this.lastLeft += event.clientX - this.coordDiffLeft;
-		this.lastTop += event.clientY - this.coordDiffTop;
-		if (this.resize) {
-			var newWidth = this.domObject.clientWidth + event.clientX - this.leftLast - this.getStyleFloatOf("padding-left", "paddingLeft") - this.getStyleFloatOf("padding-right", "paddingRight");
-			var newHeight = this.domObject.clientHeight + event.clientY - this.topLast - this.getStyleFloatOf("padding-top", "paddingTop") - this.getStyleFloatOf("padding-bottom", "paddingBottom");
-			if (this.getStyleFloatOf("min-width", "minWidth") <= newWidth && this.getStyleFloatOf("min-height", "minHeight") <= newHeight && ((this.getStyleFloatOf("max-width", "maxWidth") >= newWidth || this.getStyleFloatOf("max-width", "maxWidth") == 0) && (this.getStyleFloatOf("max-height", "maxHeight") >= newHeight || this.getStyleFloatOf("max-height", "maxHeight") == 0))) {
-				this.domObject.style.width = newWidth + "px";
-				this.domObject.style.height = newHeight + "px";
-			}
-		}
-		else if (!this.mousedInsideWindow) {
-			this.domObject.style.left = this.lastLeft + "px";
-			this.domObject.style.top = this.lastTop + "px";
-		}
-		this.coordDiffLeft = this.leftLast = event.clientX;
-		this.coordDiffTop = this.topLast = event.clientY;
-	}
-}
-windowStack.prototype.dragDisable = function () {
-	this.movable = false;
-	this.hadFocus = true;
-}
 windowStack.prototype.lostFocus = function () {
 	if (this.hadFocus) {
 		this.domObject.style.zIndex = 1;
@@ -122,12 +67,7 @@ windowStack.prototype.interceptOpacity = function () {
 	return (sampledOpacity > 0) ? sampledOpacity : 100;	/*Dirty opacity first-set for IE*/
 }
 windowStack.prototype.center = function () {
-	var docWidth = Math.max(document.documentElement.clientWidth, document.getElementsByTagName("body")[0].clientWidth);
-	var docHeight = Math.max(document.documentElement.clientHeight, document.getElementsByTagName("body")[0].clientHeight);
-	var docLeft = Math.max(document.documentElement.scrollLeft, document.getElementsByTagName("body")[0].scrollLeft);
-	var docTop = Math.max(document.documentElement.scrollTop, document.getElementsByTagName("body")[0].scrollTop);
-	this.domObject.style.left = Math.round(((docWidth - this.domObject.offsetWidth) / 2) + docLeft) + "px";
-	this.domObject.style.top = Math.round(((docHeight - this.domObject.offsetHeight) / 2) + docTop) + "px";
+	return
 }
 windowStack.prototype.hide = function () {
 	this.domObject.style.display = "none";
@@ -168,8 +108,5 @@ popupMenu.prototype.endPopup = function (event) {
 	}
 }
 popupMenu.prototype.position = function (event) {
-	if (this.open) {
-		this.menuElement.style.left = (pageXCoord(event) - 5) + "px";
-		this.menuElement.style.top = (pageYCoord(event) - 5) + "px";
-	}
+	return
 }
